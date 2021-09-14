98218e5ef6095164bb96a4414fed504189b86b98
SirMarshall <carterwalker@knights.ucf.edu>
2021-09-13 22:40:23 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,84 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Carter I. Walker
+ *
+ *  NOTE:   In the description for Exercise 20, the Example Output is conflicting with the description.
+ *          The description states a "Dunn" county with an additional tax of 0.004 , while the output
+ *          states a "Dane" county, with an additional tax of 0.005.
+ *          I assumed it would be the wisest to follow the description rather than sample output,
+ *          please forgive any confusion. Thanks!
+ */
+
+package org.example;
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //Necessary Variables
+        Scanner input = new Scanner(System.in);
+        String state,county;
+        double amount, wisconsinTax = 0.05, illinoisTax = 0.08, taxAmount, total;
+
+        //Order and state query
+        System.out.println( "What is the order amount?" );
+        amount = input.nextDouble();
+
+        System.out.println( "What state do you live in?");
+        state = input.next();
+
+        //If gate
+        if (state.equals("Wisconsin"))
+        {
+            System.out.println("What county do you live in?");
+            county = input.next();
+
+            //County tax gate
+            if (county.equals("Eau Claire"))
+            {
+                //Calculation
+                taxAmount = amount * (wisconsinTax + 0.005);
+                total = amount + taxAmount;
+
+                //Output
+                System.out.format("The tax is $%.2f", taxAmount);
+                System.out.format("The total is $%.2f", total);
+            }
+            else if (county.equals("Dunn"))
+            {
+                //Calculation
+                taxAmount = amount * (wisconsinTax + 0.004);
+                total = amount + taxAmount;
+
+                //Output
+                System.out.format("The tax is $%.2f\n", taxAmount);
+                System.out.format("The total is $%.2f", total);
+            }
+            else
+            {
+                //Calculation
+                taxAmount = amount * wisconsinTax;
+                total = amount + taxAmount;
+
+                //Output
+                System.out.format("The tax is $%.2f\n", taxAmount);
+                System.out.format("The total is $%.2f", total);
+            }
+        }
+        else if (state.equals("Illinois"))
+        {
+            //Calculation
+            taxAmount = amount * illinoisTax;
+            total = amount + taxAmount;
+
+            //Output
+            System.out.format("The tax is $%.2f\n", taxAmount);
+            System.out.format("The total is $%.2f", total);
+        }
+        else
+        {
+            System.out.format("The total is $%.2f",amount);
+        }
+    }
+}
